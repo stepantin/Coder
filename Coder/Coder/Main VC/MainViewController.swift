9985e1436badea9d14d8c291f23b8ed8380a0ec0
stepantin <120519389+stepantin@users.noreply.github.com>
2023-04-14 21:23:09 +0200

    Created Main VC

---
+++ Coder/Coder/Main VC/MainViewController.swift
@@ -0,0 +1,17 @@
+//
+//  MainViewController.swift
+//  Coder
+//
+//  Created by Константин Степанов on 14.04.2023.
+//
+
+import UIKit
+
+class MainViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+}
