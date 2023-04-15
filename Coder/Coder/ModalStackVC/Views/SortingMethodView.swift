d65330bab798853f1dce3f9c1e51567a86b976d3
stepantin <120519389+stepantin@users.noreply.github.com>
2023-04-15 21:35:47 +0200

    SortingMethodView created

---
+++ Coder/Coder/ModalStackVC/Views/SortingMethodView.swift
@@ -0,0 +1,48 @@
+//
+//  SortingMethodView.swift
+//  Coder
+//
+//  Created by Константин Степанов on 15.04.2023.
+//
+
+import UIKit
+
+class SortingMethodView: UIView {
+    
+    // MARK: - Properties
+    var dotIndicator = DotIndicator()
+    var isOn = false // state
+    
+    // MARK: - Private Properties
+    private let titleSortingMethod = UILabel()
+    private let defaults = UserDefaults.standard
+    
+    // MARK:  - Initializer
+    init(withTitle title: String) {
+        super.init(frame: .zero)
+        
+        setupView(withTitle: title)
+    }
+    
+    @available(*, unavailable)
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    // MARK: - Private Methods
+    func updateIndicatorState() {
+        dotIndicator.layer.borderWidth = isOn ? 6 : 2
+    }
+
+    // MARK: - Methods
+    private func setupView(withTitle title: String) {
+        addSubview(titleSortingMethod)
+        addSubview(dotIndicator)
+        
+        titleSortingMethod.frame = CGRect(x: 34, y: 0, width: 150, height: 20)
+        titleSortingMethod.text = title
+        titleSortingMethod.font = .systemFont(ofSize: 16)
+        titleSortingMethod.textColor = .black
+        dotIndicator.layer.borderWidth = isOn ? 6 : 2
+    }
+}
